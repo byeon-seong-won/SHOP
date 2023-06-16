960827f409b0d24617ebebd32d490809f4baaf49
Seongwon <sssingw@thezooom.kr>
2023-06-16 15:25:49 +0900

    inital commit

---
+++ data.js
@@ -0,0 +1,206 @@
+let data = 
+
+[
+  {
+    id : 0,
+    name : "THERE THERE Music Pub",
+    count : "1",
+    price : 1000
+  },
+
+  {
+    id : 1,
+    name : "lake house",
+    count : "1",
+    price : 2000
+  },
+
+  {
+    id : 2,
+    name : "Traffic Light01",
+    count : "1",
+    price : 3000
+  },
+  {
+    id : 3,
+    name : "everland01",
+    count : "1",
+    price : 4000
+  },
+
+  {
+    id : 4,
+    name : "jeong-dong-jin01",
+    count : "1",
+    price : 5000
+  },
+
+  {
+    id : 5,
+    name : "jeong-dong-jin02",
+    count : "1",
+    price : 6000
+  },
+  {
+    id : 6,
+    name : "yeon-nam-dong01",
+    count : "1",
+    price : 7000
+  },
+
+  {
+    id : 7,
+    name : "train-station",
+    count : "1",
+    price : 8000
+  },
+  {
+    id : 8,
+    name : "yeon-nam-dong02",
+    count : "1",
+    price : 9000
+  },
+
+  {
+    id : 9,
+    name : "yuldong-park02",
+    count : "1",
+    price : 10000
+  },
+
+  {
+    id : 10,
+    name : "sunset01",
+    count : "1",
+    price : 11000
+  },
+  {
+    id : 11,
+    name : "skyway",
+    count : "1",
+    price : 12000
+  },
+
+  {
+    id : 12,
+    name : "gungang-park",
+    count : "1",
+    price : 13000
+  },
+
+  {
+    id : 13,
+    name : "traffic-light02",
+    count : "1",
+    price : 14000
+  },
+  {
+    id : 14,
+    name : "christmas tree",
+    count : "1",
+    price : 15000
+  },
+
+  {
+    id : 15,
+    name : "drive",
+    count : "1",
+    price : 16000
+  },
+
+  {
+    id : 16,
+    name : "Gwanghwamun",
+    count : "1",
+    price : 17000
+  },
+  {
+    id : 17,
+    name : "crosswalk",
+    count : "1",
+    price : 18000
+  },
+
+  {
+    id : 18,
+    name : "blue sea",
+    count : "1",
+    price : 19000
+  },
+
+  {
+    id : 19,
+    name : "christmas01",
+    count : "1",
+    price : 20000
+  },
+  {
+    id : 20,
+    name : "in japan01",
+    count : "1",
+    price : 21000
+  },
+
+  {
+    id : 21,
+    name : "playground",
+    count : "1",
+    price : 22000
+  },
+
+  {
+    id : 22,
+    name : "fire",
+    count : "1",
+    price : 23000
+  },
+  {
+    id : 23,
+    name : "autumn and children",
+    count : "1",
+    price : 24000
+  },
+
+  {
+    id : 24,
+    name : "everland02",
+    count : "1",
+    price : 25000
+  },
+
+  {
+    id : 25,
+    name : "sunset02",
+    count : "1",
+    price : 26000
+  },
+  {
+    id : 26,
+    name : "itaewon",
+    count : "1",
+    price : 27000
+  },
+
+  {
+    id : 27,
+    name : "Chirstmas02",
+    count : "1",
+    price : 28000
+  },
+
+  {
+    id : 28,
+    name : "Chirstmas bear",
+    count : "1",
+    price : 29000
+  },
+  {
+    id : 29,
+    name : "in japan02",
+    count : "1",
+    price : 30000
+  }
+] 
+
+
+export default data
