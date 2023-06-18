524de32eaa2274cb25bb99e2a6ab8846eeb31321
seong <sngoneee@gmail.com>
2023-06-18 22:14:51 +0900

    장바구니 redux 수정중

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-import {Provider} from "react-redux"
+import {Provider, createStoreHook} from "react-redux"
 import store from './store.js'
 import ScrollToTop from './scrollTop.js';
+import { createStore } from "redux";
 
 
+//action 객체를 리턴하는 함수 생성
+export const increase = () => {
+  return {
+    type: "INCREASE"
+  };
+};
+
+export const decrease = () => {
+  return {
+    type: "DECREASE"
+  };
+};
+
+const count = 1;
+// reducer 생성
+const reducer = (state = count, action) => {
+  switch (action.type) {
+    //action === 'INCREASE'일 경우
+    case "INCREASE":
+      return state + 1;
+
+    // action === 'DECREASE'일 경우
+    case "DECREASE":
+      return state - 1;
+
+    // action === 'SET_NUMBER'일 경우
+    case "SET_NUMBER":
+      return action.payload;
+
+    // 해당 되는 경우가 없을 땐 기존 상태를 그대로 리턴
+    default:
+      return state;
+  }
+}
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = createRoot(rootElement);
+const store = createStore();
+
 
 root.render(
   <React.StrictMode>
@@ -28,10 +68,3 @@ root.render(
 
 
 
-
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
