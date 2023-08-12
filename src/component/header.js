c7025566a297f0f1c44e25eef0ae6600e74c3d74
seong <sngoneee@gmail.com>
2023-08-12 19:47:08 +0900

    useEffect 스크롤 이벤트 _ 0812

--- src/component/header.js
+++ src/component/header.js
@@ -1,26 +1,46 @@
+import { useEffect, useState } from 'react'
 import { useNavigate} from 'react-router-dom'
+
+
+
 const Nav = () => {
 
-    let navi = useNavigate()
-  
-    return(
-      <nav>
-        <div className='mainPop'>일상 속 다양한 순간들을 만나보세요</div>
-        <div className='navBar'>
-          <h1 onClick={()=> {navi('/')}}>wwpi.c</h1>
-          <ul>
-            <li onClick={()=> {navi('/cart')}}>
-              <i className="fa fa-shopping-cart"></i>
-              CART
-            </li>
-            <li>
-              <i className="fa fa-user"></i>
-              LOGIN
-            </li>
-          </ul>
-        </div>
-      </nav>
-    )
+  let navi = useNavigate()
+  let [scroll, setScroll] = useState('');
+
+  const handleScroll = () => {
+   let offset = window.scrollY;
+   if(offset > 10) {
+      setScroll('scroll')
+    } else {
+      setScroll('')
+    }
   }
 
-  export {Nav}
+  useEffect(()=> {
+    window.addEventListener('scroll', handleScroll);
+  },[])
+
+
+
+  return (
+    <nav className={'none ' + scroll}>
+      <div className={'mainPop ' + scroll}>일상 속 다양한 순간들을 만나보세요</div>
+      <div className='navBar'>
+        <h1 onClick={()=> {navi('/')}}>wwpi.c</h1>
+        <ul>
+          <li onClick={()=> {navi('/cart')}}>
+            <i className="fa fa-shopping-cart"></i>
+            CART
+          </li>
+          <li>
+            <i className="fa fa-user"></i>
+            LOGIN
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export {Nav}
