4a952dd72c6af96356c07b3db9541c18ac312952
Seongwon <sngoneee@gmail.com>
2023-09-04 18:31:38 +0900

    styled-component 수정

---
+++ src/component/maintabContent.js
@@ -0,0 +1,76 @@
+import styled from 'styled-components'
+
+// 하단 photo type 탭 컨텐츠
+const Tabcont = ({tab}) => {
+    return(
+      <div>
+        {[
+          <Tabimg>
+            <img src={process.env.PUBLIC_URL + '/pic_10.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_2.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_3.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_16.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_11.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_14.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_26.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_27.png'}/>
+          </Tabimg>, 
+          <Tabimg>
+            <img src={process.env.PUBLIC_URL + '/pic_5.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_6.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_4.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_19.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_21.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_30.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_31.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_32.png'}/>
+          </Tabimg>,
+          <Tabimg>
+            <img src={process.env.PUBLIC_URL + '/pic_7.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_13.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_24.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_22.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_6.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_4.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_12.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_27.png'}/>
+          </Tabimg>,
+          <Tabimg>
+            <img src={process.env.PUBLIC_URL + '/pic_17.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_9.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_1.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_8.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_18.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_12.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_23.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_24.png'}/>
+          </Tabimg>,
+          <Tabimg>
+            <img src={process.env.PUBLIC_URL + '/pic_15.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_20.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_28.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_29.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_17.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_18.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_12.png'}/>
+            <img src={process.env.PUBLIC_URL + '/pic_22.png'}/>
+          </Tabimg>
+        ][tab]
+      }
+      </div>
+    ) 
+}
+
+
+
+
+// styled components
+let Tabimg = styled.div `
+  display: grid;
+  grid-template-columns: repeat(4,1fr);
+  &>img {width: 100%;}
+`;
+
+
+
+export {Tabcont}
